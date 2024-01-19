d643e26874a70f5e6e0174a50755888ff2188bb6
Oleksandr Horbunov <sanyo.gorbunov@gmail.com>
2024-01-19 13:59:06 +0100

    Add deployment projects

---
+++ CrownForecaster.Deployment/CrownForecaster.Deployment.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace CrownForecaster.Deployment.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ CrownForecaster.Deployment/CrownForecaster.Deployment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CrownForecaster.Deployment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
