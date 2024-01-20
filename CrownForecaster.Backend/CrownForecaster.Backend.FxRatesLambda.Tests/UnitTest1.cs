87d424c122abe52ec60f0f6e406ed51c83616706
Oleksandr Horbunov <sanyo.gorbunov@gmail.com>
2024-01-20 11:30:15 +0100

    Add backbone of lambda

---
+++ CrownForecaster.Backend/CrownForecaster.Backend.FxRatesLambda.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace CrownForecaster.Backend.FxRatesLambda.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ CrownForecaster.Backend/CrownForecaster.Backend.FxRatesLambda/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace CrownForecaster.Backend.FxRatesLambda
+{
+    public class Class1
+    {
+
+    }
+}
